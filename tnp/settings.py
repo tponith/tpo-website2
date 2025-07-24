9ebfe8b3b3e2d620b42e369611d569e5dfc82de1
AceHunterr <aggarwalmehul26@gmail.com>
2025-07-24 20:23:46 +0530

    Final changes for 2026 batch

--- tnp/settings.py
+++ tnp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'vpn',
+    # 'database',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'tnp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
